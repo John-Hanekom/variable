9630768584578638f6e02b4d6051b31a6549026a
John-Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-09-26 09:23:12 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,19 @@
+def on_button_pressed_a():
+    global counter
+    counter += 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_number(counter)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global counter
+    counter += -1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# Created by: John Hanekom
+# Date: September 26th, 2022
+# This program sets a variable named "counter" to 0 and adds 1 when you press A but subtracts 1 when you press B. When you press A+B it will show the number
+counter = 0
+counter = 0
---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
+// Created by: John Hanekom
+// Date: September 26th, 2022
+// This program sets a variable named "counter" to 0 and adds 1 when you press A but subtracts 1 when you press B. When you press A+B it will show the number
+let counter = 0
+counter = 0
