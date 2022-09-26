313063b59fdf655129ab790ba4771306a81cec32
John-Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-09-26 09:26:49 -0400

    Added a new line in a comment

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 # Created by: John Hanekom
 # Date: September 26th, 2022
+# 
 # This program sets a variable named "counter" to 0 and adds 1 when you press A but subtracts 1 when you press B. When you press A+B it will show the number
 counter = 0
 counter = 0
--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onButtonPressed(Button.B, function () {
 })
 // Created by: John Hanekom
 // Date: September 26th, 2022
+// 
 // This program sets a variable named "counter" to 0 and adds 1 when you press A but subtracts 1 when you press B. When you press A+B it will show the number
 let counter = 0
 counter = 0
